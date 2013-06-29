fd23f7720849aff5815e56c125f0e17694b71b2a
Alexander Scheurer <alexander.scheurer@hs-furtwangen.de>
2013-06-29 23:09:02 +0200

    Added Player class, collisions should be working, the rest is dummy code. Cleaned up GameEntity/-Handler and Main.cs

---
+++ projects/TheGame/Entities/Player.cs
@@ -0,0 +1,41 @@
+﻿using Fusee.Engine;
+using Fusee.Math;
+
+namespace Examples.TheGame.Entities
+{
+    public class Player : GameEntity
+    {
+        private int _life;
+        private Time _lastShotTime;
+
+
+        public Player(int id, Mesh mesh, float collisionRadius, float4x4 position, float speed, float impact)
+            : base(id, mesh, collisionRadius, position, speed, impact)
+        {
+        }
+
+        public int GetLife()
+        {
+            return _life;
+        }
+        public void Collision()
+        {
+            foreach (var go in GameHandler.Players)
+            {
+                float4x4 distanceMatrix = float4x4.Substract(go.Value.GetPosition(), this.GetPosition());
+                var distance = (float)System.Math.Sqrt((System.Math.Pow((double)distanceMatrix.M41, 2) + System.Math.Pow((double)distanceMatrix.M42, 2) + System.Math.Pow((double)distanceMatrix.M43, 2)));
+                var distancecoll = go.Value.GetCollisionRadius() + this.GetCollisionRadius();
+
+                if (distance < distancecoll)
+                {
+                    //BOOOOM
+                }
+            }
+        }
+        public void Shoot()
+        {
+
+        }
+    }
+
+}
--- projects/TheGame/GameHandler.cs
+++ projects/TheGame/GameHandler.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using Examples.TheGame.Entities;
-using Examples.TheGame.GameStates;
 using Fusee.Engine;
 
 namespace Examples.TheGame
@@ -11,95 +10,24 @@ namespace Examples.TheGame
     /// </summary>
     public class GameHandler
     {
-        /// <summary>
-        ///     A dictionary storing all game states that are known to the game
-        /// </summary>
- 
-        
+      
         /// <summary>
         ///Disctionarires mit allen Items und Playern
         /// </summary>
-        public Dictionary<int, GameEntity> _items;
-        public Dictionary<int, GameEntity> _players;
-
-
-
-        private readonly Dictionary<GameState, AbstractGameState> _gameStates;
+        public Dictionary<int, GameEntity> Items;
+        public static Dictionary<int, Player> Players;
 
         /// <summary>
         ///     RenderContext
         /// </summary>
         private readonly RenderContext _rc;
 
-        /// <summary>
-        ///     Currently active and updated game state
-        /// </summary>
-        private AbstractGameState _curGameState;
-
-        /// <summary>
-        ///     Initializes a new instance of the <see cref="GameHandler" /> class.
-        /// </summary>
-        /// <param name="rc">Render context of the game window</param>
         public GameHandler(RenderContext rc)
         {
             //pass RenderContext
             _rc = rc;
-            _items = new Dictionary<int, GameEntity> ();
-            _players = new Dictionary<int, GameEntity>();
-
-            // TODO: NetworkManager initalize
-
-            // Initialize list of all game states
-            _gameStates = new Dictionary<GameState, AbstractGameState>();
-            InitStatesList(_gameStates);
-        }
-
-        /// <summary>
-        ///     Inits the list of all states the game knows
-        /// </summary>
-        /// <param name="list">The list to add the states to</param>
-        private void InitStatesList(Dictionary<GameState, AbstractGameState> list)
-        {
-            // TODO: Add all other game states that are being implemented
-            list.Add(GameState.InGame, new InGameState());
-
-            // Set the initial game state to use
-            _curGameState = _gameStates[GameState.InGame];
-        }
-
-        /// <summary>
-        ///     Update entity states, is called once per frame
-        /// </summary>
-        public void UpdateStates()
-        {
-            if (_curGameState != null)
-                _curGameState.Update();
-        }
-
-        /// <summary>
-        ///     Is called once per frame to updeate rendering
-        /// </summary>
-        public void RenderAFrame()
-        {
-            if (_curGameState != null)
-                _curGameState.Render(_rc);
-        }
-
-        /// <summary>
-        ///     Switches the state of the game.
-        /// </summary>
-        /// <param name="gameState">State of the game.</param>
-        /// <exception cref="ArgumentException">The passed game state is not known.</exception>
-        public void SwitchGameState(GameState gameState)
-        {
-            if (_gameStates.ContainsKey(gameState))
-            {
-                _curGameState = _gameStates[gameState];
-            }
-            else
-            {
-                throw new ArgumentException("Tried to switch the game to a state that doesn't exist or is not known.");
-            }
+            Items = new Dictionary<int, GameEntity>();
+            Players = new Dictionary<int, Player>();
         }
     }
 }
